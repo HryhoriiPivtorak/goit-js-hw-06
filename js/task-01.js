4d0aabd70f8eb173a8fb5dec6354eac2d994875d
Hryhorii Pivtorak <grigoriypivtorak@gmail.com>
2022-12-10 18:46:11 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -3,8 +3,10 @@ console.log('Number of categories: ', listUl.children.length);
 
 console.log('');
 
-const firstElem = listUl.children[0].children[0].textContent;
-const numberOfFirstElem = listUl.children[0].children[1].children.length;
+const listLi = document.querySelectorAll('.item');
+
+const firstElem = listLi[0].children[0].textContent;
+const numberOfFirstElem = listLi[0].children[1].children.length;
 
 console.log('Category: ', firstElem);
 console.log('Elements: ', numberOfFirstElem);
@@ -12,16 +14,16 @@ console.log('Elements: ', numberOfFirstElem);
 
 console.log('');
 
-const secondElem = listUl.children[1].children[0].textContent;
-const numberOfSecondElem = listUl.children[1].children[1].children.length;
+const secondElem = listLi[1].children[0].textContent;
+const numberOfSecondElem = listLi[1].children[1].children.length;
 
 console.log('Category: ', secondElem);
 console.log('Elements: ', numberOfSecondElem);
 
 console.log('');
 
-const thirdElem = listUl.children[2].children[0].textContent;
-const numberOfthirdElem = listUl.children[2].children[1].children.length;
+const thirdElem = listLi[2].children[0].textContent;
+const numberOfthirdElem = listLi[2].children[1].children.length;
 
 console.log('Category: ', thirdElem);
 console.log('Elements: ', numberOfthirdElem);
