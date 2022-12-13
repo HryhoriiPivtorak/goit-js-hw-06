26bc05520003a368bdbb398feb17790e554c4110
Hryhorii Pivtorak <grigoriypivtorak@gmail.com>
2022-12-13 22:45:19 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,16 +1,19 @@
 const input = document.querySelector("#validation-input");
 
-console.log(input.dataset.length);
-
 input.addEventListener("blur", onInputBlur);
 
 function onInputBlur(event) {
+  console.log(input.dataset.length);
+  console.log(event.currentTarget.value.length);
+
   if (event.currentTarget.value.length == input.dataset.length) {
     input.setAttribute("id", "validation-input.valid");
-  }
-  if (event.currentTarget.value.length === 0) {
+    console.log("Valid");
+  } else if (event.currentTarget.value.length === 0) {
     input.setAttribute("id", "validation-input");
+    console.log("Basic");
   } else {
     input.setAttribute("id", "validation-input.invalid");
+    console.log("Invalid");
   }
 }
