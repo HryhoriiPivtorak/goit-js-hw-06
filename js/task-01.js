2eadb789c938729711214a34b226135e92c0a5aa
Hryhorii Pivtorak <grigoriypivtorak@gmail.com>
2022-12-13 22:27:13 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -3,12 +3,11 @@ console.log("Number of categories: ", listUl.children.length);
 
 const listLi = document.querySelectorAll(".item");
 
-for (let i = 0; i < listUl.children.length; i += 1) {
-  const category = listLi[i].children[0].textContent;
-  const elements = listLi[i].children[1].children.length;
-
+listLi.forEach(elem => {
+  const category = elem.children[0].textContent;
+  const elements = elem.children[1].children.length;
   console.log("");
 
   console.log("Category: ", category);
   console.log("Elements: ", elements);
-}
+})
