4799657e9ca0bdddc7acc64ca6d0643fb3b50e84
Hryhorii Pivtorak <grigoriypivtorak@gmail.com>
2022-12-15 20:07:28 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit (event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+
+    if (formElements.email.value && formElements.password.value) {
+        const array = {
+            email: formElements.email.value,
+            password: formElements.password.value
+        }
+        console.log ('array: ', array);
+        form.reset();
+    } else {
+        alert('Please, complete all fields!')
+    }
+   
+}
