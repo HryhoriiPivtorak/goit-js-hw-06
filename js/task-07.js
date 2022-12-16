9170724ecb895d38c96639898fb57f02edfd0a34
Hryhorii Pivtorak <grigoriypivtorak@gmail.com>
2022-12-16 22:12:14 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -2,7 +2,7 @@ const input = document.querySelector('#font-size-control');
 const textSpan = document.querySelector('#text');
 
 
-input.addEventListener('click', onClick);
+input.addEventListener('input', onClick);
 
 function onClick(event) {
     textSpan.style.fontSize = `${input.value}px`;
