63553b433937a71c04a383a2168f5b6d078f5418
Hryhorii Pivtorak <grigoriypivtorak@gmail.com>
2022-12-16 22:21:59 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,15 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-  
-const button = document.querySelector('button.change-color');
-const body =document.querySelector('body');
-const span = document.querySelector('.color');
 
-button.addEventListener('click', onClick);
+const button = document.querySelector("button.change-color");
+const body = document.querySelector("body");
+const span = document.querySelector(".color");
+
+button.addEventListener("click", onClick);
 
 function onClick(event) {
-body.style.backgroundColor = getRandomHexColor();
-span.textContent = getRandomHexColor();
+  let randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  span.textContent = randomColor;
 }
-
